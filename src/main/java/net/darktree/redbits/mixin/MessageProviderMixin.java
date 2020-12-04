54c8916ae538ac377ce416a6d35c6e9706cf6743
magistermaks <magistermaks@darktree.net>
2020-12-04 16:25:09 +0100

    Added nothing

---
+++ src/main/java/net/darktree/redbits/mixin/MessageProviderMixin.java
@@ -0,0 +1,31 @@
+package net.darktree.redbits.mixin;
+
+import net.minecraft.client.resource.SplashTextResourceSupplier;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.profiler.Profiler;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(SplashTextResourceSupplier.class)
+abstract public class MessageProviderMixin {
+
+    // Nothing to see here //
+
+    @Final
+    private final List<String> splashTexts = new ArrayList<>();
+
+    @Inject(at = @At("TAIL"), method = "apply(Ljava/util/List;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V")
+    private void apply(List<String> list, ResourceManager resourceManager, Profiler profiler, CallbackInfo info) {
+        splashTexts.add("I'm the man who arranges the blocks!");
+        splashTexts.add("Piracy is all about branding!");
+        splashTexts.add("And what can you do, my effeminate fellow?");
+        splashTexts.add("Check out Sequensa Programming Language!");
+    }
+
+}
