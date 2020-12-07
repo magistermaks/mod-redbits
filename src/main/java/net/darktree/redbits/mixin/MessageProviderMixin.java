10065f5162dd0d0172051e1a32741d98c79d2659
magistermaks <magistermaks@darktree.net>
2020-12-07 16:07:04 +0100

    Mixin fix

--- src/main/java/net/darktree/redbits/mixin/MessageProviderMixin.java
+++ src/main/java/net/darktree/redbits/mixin/MessageProviderMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,6 +19,7 @@ abstract public class MessageProviderMixin {
     // Nothing to see here //
 
     @Final
+    @Shadow
     private final List<String> splashTexts = new ArrayList<>();
 
     @Inject(at = @At("TAIL"), method = "apply(Ljava/util/List;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V")
