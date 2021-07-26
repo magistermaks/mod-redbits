a2733c1bd55e739ec661ec5ceb295d3bce04b00a
magistermaks <magistermaks@darktree.net>
2021-07-26 23:26:31 +0200

    Fixes, bumped version

--- src/main/java/net/darktree/redbits/mixin/MessageProviderMixin.java
+++ src/main/java/net/darktree/redbits/mixin/MessageProviderMixin.java
@@ -6,10 +6,12 @@ import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Base64;
 import java.util.List;
 
 @Mixin(SplashTextResourceSupplier.class)
@@ -21,13 +23,19 @@ abstract public class MessageProviderMixin {
     @Shadow
     private List<String> splashTexts;
 
+    @Unique
+    private void supply( String data ) {
+        splashTexts.add( new String( Base64.getDecoder().decode(data) ) );
+    }
+
     @Inject(at = @At("TAIL"), method = "apply(Ljava/util/List;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V")
     private void apply(List<String> list, ResourceManager resourceManager, Profiler profiler, CallbackInfo info) {
-        splashTexts.add("I" + "'m the man who arranges the blocks!");
-        splashTexts.add("P" + "iracy is all about branding!");
-        splashTexts.add("A" + "nd what can you do, my effeminate fellow?");
-        splashTexts.add("C" + "heck out Sequensa Programming Language!");
-        splashTexts.add("Y" + "ou know the rules and so do I!");
+        supply("SSdtIHRoZSBtYW4gd2hvIGFycmFuZ2VzIHRoZSBibG9ja3Mh");
+        supply("UGlyYWN5IGlzIGFsbCBhYm91dCBicmFuZGluZyE=");
+        supply("QW5kIHdoYXQgY2FuIHlvdSBkbywgbXkgZWZmZW1pbmF0ZSBmZWxsb3c/");
+        supply("Q2hlY2sgb3V0IFNlcXVlbnNhIFByb2dyYW1taW5nIExhbmd1YWdlIQ==");
+        supply("WW91IGtub3cgdGhlIHJ1bGVzIGFuZCBzbyBkbyBJIQ==");
+        supply("Q2hlY2sgb3V0IERhc2hMb2FkZXIh");
     }
 
 }
