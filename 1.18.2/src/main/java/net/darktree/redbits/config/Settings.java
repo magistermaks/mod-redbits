656ad37a099169684112eb7cb289da0bd30cf1da
magistermaks <magistermaks@darktree.net>
2024-04-06 21:45:48 +0200

    Added tooltips to the setting screen

--- 1.18.2/src/main/java/net/darktree/redbits/config/Settings.java
+++ 1.18.2/src/main/java/net/darktree/redbits/config/Settings.java
@@ -7,11 +7,17 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 @Config(name = "redbits")
 public class Settings implements ConfigData {
 
+	@ConfigEntry.Gui.Tooltip(count=2)
 	public boolean disable_burnout = true;
+
+	@ConfigEntry.Gui.Tooltip(count=2)
 	public boolean jukebox_integration = true;
+
+	@ConfigEntry.Gui.Tooltip(count=2)
 	public boolean campfire_integration = true;
 
 	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.Gui.Tooltip(count=2)
 	public boolean add_guide_to_loot_tables = true;
 
 }
