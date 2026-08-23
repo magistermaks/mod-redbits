b9c64bf48cb8e12700ecca5fd49c031fe362bf67
magistermaks <magistermaks@darktree.net>
2026-08-24 00:14:44 +0200

    Fix messages

--- src/main/java/net/darktree/redbits/RedBitsClient.java
+++ src/main/java/net/darktree/redbits/RedBitsClient.java
@@ -44,24 +44,6 @@ public class RedBitsClient implements ClientModInitializer {
 		// minecart renderer
 		EntityRendererRegistry.register(RedBits.EMITTER_MINECART, ctx -> new MinecartEntityRenderer(ctx, EntityModelLayers.TNT_MINECART));
 
-		// nothing to see here
-		// TODO
-//		MessageInjector.inject("SSdtIHRoZSBtYW4gd2hvIGFycmFuZ2VzIHRoZSBibG9ja3Mh");
-//		MessageInjector.inject("UGlyYWN5IGlzIGFsbCBhYm91dCBicmFuZGluZyE=");
-//		MessageInjector.inject("QW5kIHdoYXQgY2FuIHlvdSBkbywgbXkgZWZmZW1pbmF0ZSBmZWxsb3c/");
-//		MessageInjector.inject("Q2hlY2sgb3V0IFNlcXVlbnNhIFByb2dyYW1taW5nIExhbmd1YWdlIQ==");
-//		MessageInjector.inject("WW91IGtub3cgdGhlIHJ1bGVzIGFuZCBzbyBkbyBJIQ==");
-//		MessageInjector.inject("U3RhbmQgd2l0aCBVa3JhaW5lIQ==");
-//		MessageInjector.inject("VGhlIG5vYmxlIGJlbmVmYWN0b3JzPyBHb25lLg==");
-//		MessageInjector.inject("MDkgRjkh");
-//		MessageInjector.inject("WW91IHdvbid0IGV2ZW4gZGllIGhvcnJpYmx5IQ==");
-//		MessageInjector.inject("TW9zdCBWZXhpbmcgUGFyc2Uh");
-//		MessageInjector.inject("QWxzbyBUcnkgU3BhY2UgU2hpZnRlciE=");
-//
-//		if (!PatchouliProxy.isModLoaded()) {
-//			MessageInjector.inject("VHJ5IHdpdGggUGF0Y2hvdWxpIQ==");
-//		}
-
 		ClientTickEvents.END_WORLD_TICK.register(world -> {
 			PlayerEntity player = MinecraftClient.getInstance().player;
 
