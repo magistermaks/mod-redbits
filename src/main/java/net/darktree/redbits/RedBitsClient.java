44e8604ed50e850dac6f0060647e1dd6d75fc0b6
magistermaks <magistermaks@darktree.net>
2026-08-23 23:18:32 +0200

    Fix item tints

--- src/main/java/net/darktree/redbits/RedBitsClient.java
+++ src/main/java/net/darktree/redbits/RedBitsClient.java
@@ -38,9 +38,7 @@ public class RedBitsClient implements ClientModInitializer {
 		cutout(RedBits.PROJECTOR);
 		cutout(RedBits.JUNCTION);
 
-//		FIXME ColorProviderRegistry.ITEM.register((stack, tintIndex) -> RedstoneWireBlock.getWireColor(1), RedBits.REDSTONE_EMITTER);
 		ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> RedstoneWireBlock.getWireColor(state.get(EmitterBlock.POWER)), RedBits.REDSTONE_EMITTER);
-//		FIXME ColorProviderRegistry.ITEM.register((stack, tintIndex) -> ColorProvider.getColor(0), RedBits.RGB_LAMP);
 		ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> ColorProvider.getColor(state.get(AnalogLampBlock.POWER)), RedBits.RGB_LAMP);
 
 		// minecart renderer
